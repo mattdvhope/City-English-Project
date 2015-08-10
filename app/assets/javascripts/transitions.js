ba16989c0a2719b828e1ede830ddc24311603d44
mattdvhope <mattagape@me.com>
2015-08-11 06:38:59 +0700

    c

--- app/assets/javascripts/transitions.js
+++ app/assets/javascripts/transitions.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
   $("#place_1").addClass("active");
 
   $(".page-scroll").on("click", function() {
+    event.preventDefault();
     $("#place_1").removeClass("active");
     $("#place_2").removeClass("active");
     $("#place_3").removeClass("active");
@@ -10,13 +11,54 @@ $(document).ready(function() {
     $(this).addClass("active");
   });
 
-
-  $('a').on("click", function(){
-    $('html, body').animate({
-      scrollTop: $( $.attr(this, 'href') ).offset().top
-    }, 550, 'swing').attr('href');
-    return false;
+  $('a[href^="#"]').on('click',function (e) {
+    e.preventDefault();
+      $('html, body').animate({
+        scrollTop: $($(this).attr('href')).offset().top
+      }, 300, 'swing');
   });
+
+  // $('a').on("click", function(){
+  //   console.log($(this).offset());
+  //   $('html, body').stop().animate({
+  //     scrollTop: 3   //$(this).offset().top
+  //   }, 550, 'swing');
+  //   // return false;
+  // });
+
+
+  // var body = $("html, body");
+  // body.stop().animate({scrollTop:0}, '500', 'swing', function() { 
+  //    alert("Finished animating");
+  // });
+
+  // $('a').click(function(e) {
+  //   var target = $(this).attr('href');
+  //   e.preventDefault();
+  //   console.log($(target).offset());
+  //   $('html,body').animate({
+  //     scrollTop: $(target).offset().top
+  //   }, 800, 'swing');
+  // });
+
+
+  // $('a[href^="#"]').on('click',function (e) {
+  //   e.preventDefault();
+
+  //   var target = this,
+  //   $target = $(target);
+  //   console.log(typeof $target.offset().top);
+
+  //   $('html, body').stop().animate({
+  //     scrollTop: $target.offset().top
+  //   }, 900, 'swing', function () {
+  //     window.location.href = $target.offset().top;
+  //   });
+
+  // });
+
+
+
 
 });
 
