e2129c0dd88e170339eb5f131d3ecb8c120e234b
mattdvhope <mattagape@me.com>
2015-08-10 22:35:00 +0700

    ch

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,9 @@ module CityEnglish
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    config.assets.precompile = config.assets.precompile + %w(*.png *.jpg *.jpeg *.gif *.woff *.ttf *.svg *.eot)
+    # config.assets.precompile += %w( .svg .eot .woff .ttf )
+
+    config.assets.precompile + %w(*.png *.jpg *.jpeg *.gif *.woff *.ttf *.svg *.eot)
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
