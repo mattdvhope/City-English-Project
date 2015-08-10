0f5fe744c348322ccd0b18837c4fd8ef5346c20e
mattdvhope <mattagape@me.com>
2015-08-10 20:58:00 +0700

    c

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,9 @@ module CityEnglish
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    # for glyphicons
+    config.assets.precompile += %w( *.eot *.svg *.ttf *.woff *.otf)
+
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
